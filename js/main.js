bbb1d12e9b125b60b0ba979d71ea47da808d8601
diaamohamed013 <diaamohamed013@gmail.com>
2022-07-03 02:34:31 +0200

    new-update2

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
-// $(".nav-item").on("click", function () {
-//     $(this).addClass("active");
-//     $(this).siblings().removeClass("active");
-// });
+$(".nav-item").on("click", function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+});
 
-// $(`.navbar-nav li a[href^="#"]`).click(function(e){
-//  //scrolling to section after click its link from nav bar
-//     let aHref = $(e.target).attr("href");
-//     let lin = $(aHref).offset().top;
-//     $('body,html').animate({
-//             scrollTop: lin
-//         }, 100);
-// })
-
-$(document).ready(function () {
-    //Smooth scrolling when click to nav
-    // $('.navbar-nav li a[href^="#"]').click(function (e) {
-    //     e.preventDefault();
-    //     var curLink = $(this);
-    //     var scrollPoint = $(curLink.attr('href')).offset().top;
-    //     $('body,html').animate({
-    //         scrollTop: scrollPoint
-    //     }, 100);
-    // })
-
-    $(`.navbar-nav li a[href^="#"]`).click(function (e) {
-        //scrolling to section after click its link from nav bar
-        let aHref = $(e.target).attr("href");
-        let lin = $(aHref).offset().top;
-        $('body,html').animate({
+$(`.navbar-nav li a[href^="#"]`).click(function(e){
+ //scrolling to section after click its link from nav bar
+    let aHref = $(e.target).attr("href");
+    let lin = $(aHref).offset().top;
+    $('body,html').animate({
             scrollTop: lin
         }, 100);
-    })
+})
 
-    $(window).scroll(function () {
-        onScrollHandle();
-    });
+// $(document).ready(function () {
+//     //Smooth scrolling when click to nav
+//     // $('.navbar-nav li a[href^="#"]').click(function (e) {
+//     //     e.preventDefault();
+//     //     var curLink = $(this);
+//     //     var scrollPoint = $(curLink.attr('href')).position().top;
+//     //     $('body,html').animate({
+//     //         scrollTop: scrollPoint
+//     //     }, 100);
+//     // })
 
-    function onScrollHandle() {
+//     $(`.navbar-nav li a[href^="#"]`).click(function (e) {
+//         //scrolling to section after click its link from nav bar
+//         let aHref = $(e.target).attr("href");
+//         let lin = $(aHref).offset().top;
+//         $('body,html').animate({
+//             scrollTop: lin
+//         }, 100);
+//     })
 
-        //Get current scroll position
-        var currentScrollPos = $(document).scrollTop();
+//     $(window).scroll(function () {
+//         onScrollHandle();
+//     });
 
-        //Iterate through all node
-        $('.navbar-nav a').each(function () {
-            var curLink = $(this);
-            var refElem = $(curLink.attr('href'));
-            //Compare the value of current position and the every section position in each scroll
-            if (refElem.offset().top <= currentScrollPos && refElem.offset().top + refElem.height() > currentScrollPos) {
-                //Remove class active in all nav
-                $('.navbar-nav > ul > li').removeClass("active");
-                //Add class active
-                curLink.parent().addClass("active");
-            }
-            else {
-                curLink.parent().removeClass("active");
-            }
-        });
-    }
-});
+//     function onScrollHandle() {
+
+//         //Get current scroll position
+//         var currentScrollPos = $(document).scrollTop();
+
+//         //Iterate through all node
+//         $('.navbar-nav a').each(function () {
+//             var curLink = $(this);
+//             var refElem = $(curLink.attr('href'));
+//             //Compare the value of current position and the every section position in each scroll
+//             if (refElem.position().top <= currentScrollPos && refElem.position().top + refElem.height() > currentScrollPos) {
+//                 //Remove class active in all nav
+//                 $('.navbar-nav > ul > li').removeClass("active");
+//                 //Add class active
+//                 curLink.parent().addClass("active");
+//             }
+//             else {
+//                 curLink.parent().removeClass("active");
+//             }
+//         });
+//     }
+// });
